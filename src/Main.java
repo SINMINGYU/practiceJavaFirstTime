d4917d614151a10ef43f684eb30fa10b23d6d143
sin min gyu <smgms7513@gmail.com>
2023-03-29 23:51:26 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 10;
+        System.out.println("i = " + i);
+
+        i = 100;
+        System.out.println("i = " + i);
+
+        i = 300;
+        System.out.println("i = " + i);
+
+        i = 0;
+        System.out.println("i = " + i);
+    }
+}

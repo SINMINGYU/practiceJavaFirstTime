40665a2cd06055015acef1e763d7676ecec8a1a0
sin min gyu <smgms7513@gmail.com>
2023-03-31 23:56:44 +0900

    third commit

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,16 @@ public class Main {
 
         i = 125;
         System.out.println("i = " + i);
+
+        System.out.println("오늘의 날씨는 10도입니다.");
+        System.out.printf("오늘의 날씨는 %d도입니다.\n", 8);
+
+        char c1 = 'a';
+        char c2 = 'A';
+
+        System.out.printf("소문자 \'%c\'의 대문자는 \'%c\'입니다.\n", c1, c2);
+
+        double d = 1.23456d;
+        System.out.printf("d = %f\n", d);
     }
 }
